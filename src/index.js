a71222109418a433a8302a0b8ea02e12167c38d3
caio <caio@MacBook-Pro-de-caio.local>
2021-03-01 21:21:22 -0300

    FreechangeProductBar Styles

--- src/index.js
+++ src/index.js
@@ -9,15 +9,11 @@ import './index.css'
 import './components/Header.css';
 import Header from './components/Header';
 
-import './components/FreeChangeProductBar.css';
-import FreeChangeProduct from './components/FreeChangeProductBar'
-
 import './pages/Product.css';
 import Product from './pages/Product';
 
 ReactDOM.render(
   <React.StrictMode>
-    <FreeChangeProduct />
     <Header />
     <Product />
   </React.StrictMode>,
